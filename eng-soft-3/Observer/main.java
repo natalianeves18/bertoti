5ef0231ae3844b87ee0d67b99210cb055cc01d03
Natália Reis Neves <51200773+natalianeves18@users.noreply.github.com>
2023-03-29 22:25:57 -0300

    Adição de novos parâmetros de teste

--- eng-soft-3/Observer/main.java
+++ eng-soft-3/Observer/main.java
@@ -1,13 +1,18 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
         Pontuacao pontuacao = new Pontuacao();
         Jogador jogador1 = new Jogador("Jogador 1");
         Jogador jogador2 = new Jogador("Jogador 2");
         Jogador jogador3 = new Jogador("Jogador 3");
+        Jogador jogador4 = new Jogador("Jogador 4");
+
         pontuacao.adicionarObservador(jogador1);
         pontuacao.adicionarObservador(jogador2);
         pontuacao.adicionarObservador(jogador3);
+        pontuacao.adicionarObservador(jogador4);
+
         pontuacao.setPontuacao(10);
         pontuacao.setPontuacao(20);
+        pontuacao.setPontuacao(30);
     }
 }
